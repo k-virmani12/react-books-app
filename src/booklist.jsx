9e2cffcbbdfe07237a52985c93119041fd50b982
Kunal Virmani <kunal@antarx.tech>
2020-02-21 18:00:18 +0530

    added navbar and booklist along with some styling

---
+++ src/booklist.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function BookList () {
+	return (
+		<div className = "book-list">
+			<ul>
+				<li>The way of Kings</li>
+				<li>The name of the wind</li>
+				<li>the final empire</li>
+			</ul>
+		</div>
+		)
+}
---
+++ src/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Nav () {
+	return (
+		<nav>
+			<h1>Book App</h1>
+			<ul className = "nav-items">
+				<li>Home</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+		)
+}
