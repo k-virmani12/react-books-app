063f653c5d2564a7ca345e0affbcbae68d0930a8
Kunal Virmani <kunal@antarx.tech>
2020-02-22 13:12:42 +0530

    added context provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Nav from './navbar';
 import BookList from './booklist';
+import ThemeContextProvider from "./context/context-provider"
 
 function App() {
 	return (
 		<div className="App">
-			<Nav />
-			<BookList />
+			<ThemeContextProvider>
+				<Nav />
+				<BookList />
+			</ThemeContextProvider>
 		</div>
 		);
 }
