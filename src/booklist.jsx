25e86bcee049b562f3bfcbb5cd187c84e7cbcf21
Kunal Virmani <kunal@antarx.tech>
2020-02-23 00:32:47 +0530

    added context to BookList nav component

--- src/booklist.jsx
+++ src/booklist.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ThemeContext } from './context/context-provider';
 
-export default function BookList () {
-	return (
-		<div className = "book-list">
-			<ul>
-				<li>The way of Kings</li>
-				<li>The name of the wind</li>
-				<li>the final empire</li>
-			</ul>
-		</div>
-		)
+class BookList extends Component {
+	static contextType = ThemeContext;
+	render () {
+		const { isLightTheme, light, dark } = this.context;
+		const theme = isLightTheme ? light : dark;
+		return (
+			<div 
+				className = "book-list"
+				style = {{ background : theme.bg, color : theme.syntax }}>
+				<ul>
+					<li style = {{ background : theme.ui }}>The way of Kings</li>
+					<li style = {{ background : theme.ui }}>The name of the wind</li>
+					<li style = {{ background : theme.ui }}>the final empire</li>
+				</ul>
+			</div>
+			)
+	}
 }
+
+export default BookList;
